016cb664315beccf5eef904a30bd4c14db6baf8a
jundl77 <julianbrendl@gmail.com>
2016-11-21 11:18:08 +0100

    trying more new things

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,15 @@
-var webpack = require('webpack');
-
 module.exports = {
     entry: "./src/index.js",
     target: 'node',
     module: {
         loaders: [
             {test: /\.json$/, loader: "json-loader"},
-            {test: /\.jsx?$/, exclude: /(node_modules)/, loader: 'babel-loader'}
+            {test: /\.jsx?$/, exclude: /(node_modules)/, loader: 'babel'}
         ]
     },
     output: {
         path: "./lib",
-        filename: "lib.min.js"
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': '"production"'
-        }),
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({mangle: false, sourcemap: false})
-    ]
+        filename: "lib.min.js",
+        libraryTarget: 'umd'
+    }
 };
